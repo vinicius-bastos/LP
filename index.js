4d92c9d509102a146dceac317f3374216010cb8d
vinicius-bastos <61800171+vinicius-bastos@users.noreply.github.com>
2021-01-22 10:13:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars")
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
+const Post = require('./models/Post')
 
 
 // Config   
@@ -11,22 +11,30 @@ const Sequelize = require('sequelize')
     app.set('view engine', 'handlebars')
 
 //body-parser
-    app.use(bodyParser.urlencoded({extend:false}))
+    app.use(bodyParser.urlencoded({extended:false}))
     app.use(bodyParser.json())
 
-//conexão com o db mysql
-    const sequelize = new Sequelize('teste', 'root', 'bandtec', {
-    host: 'localhost',
-    dialect: 'mysql' 
-})
 
 //rotas
+    app.get('/', function(req, res){
+            Post.findAll().then(function(posts){
+            res.render('home', {nome: "Vinicius", sobrenome: "Carvalho"})
+        })
+    })
+
     app.get('/cad', function(req, res){
         res.render('formulario')
     })
 
     app.post('/add', function(req, res){
-        res.send("Texto: "+ req.body.titulo+ " Conteudo: "+ req.body.conteudo)
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/')
+        }).catch(function(erro){
+            res.send("Houve um erro" + erro)
+        })
     })
 
 app.listen(8081, function(){
